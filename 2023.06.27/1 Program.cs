b331eadb0cadaa31d520b4beb265cb79473c4952
NavinKetipe <135522914+NavinKetipe@users.noreply.github.com>
2023-06-28 11:52:31 +0530

    Add files via upload

---
+++ 2023.06.27/1 Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void main(string[] args)
+        {
+            int[] num = new int[5];
+            int sum = 0;
+            for (int i = 0; i < num length; i++)
+            {
+                console.writeLine("Enter value : ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < num length; i++)
+            {
+                sum = sum + num[i];
+            }
+
+            {
+                console.Readline();
+            }
+        }
+    }
+}
---
+++ 2023.06.27/2 Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void main(string[] args)
+        {
+            int[] num = { 122, 45, 65, 56, 49, 326, 15, 45, 945, 153 };
+
+            int smallest = num[0];
+            int biggest = num[0];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (smallest > num[i])
+                {
+                    smallest = num[i];
+                }
+                if (biggest < num[i])
+                {
+                    biggest = num[i];
+                }
+            }
+            console.WriteLine("smallest" + smallest);
+            console.WriteLine("biggest" + biggest);
+        }
+    }
+}
