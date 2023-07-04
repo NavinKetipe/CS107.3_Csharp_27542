9930c6b5adee1f2e46efeab51d11bce0ee99cc39
NavinKetipe <135522914+NavinKetipe@users.noreply.github.com>
2023-07-04 11:08:07 +0530

    Add files via upload

---
+++ 2023.07.04/Q6 1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class ArrayClass
+{
+	public int[] arrayMethod(int size)
+	{
+		int[] array = new int[size];
+		for(int i=0;i<array.length;i+=2)
+		{
+			console.WriteLine("Enter a value:");
+			int value = Convert.ToInt32(console.ReadLine());
+
+			array[i] = value;
+			array[ ] =(int)value
+		}
+	}
+}
---
+++ 2023.07.04/Q6 1/Q6 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6_1
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the size of the array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            ArrayClass arrayObj = new ArrayClass();
+            int[] array = arrayObj.arrayMethod(size);
+
+            Console.WriteLine("\nDispaly the array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+   
